6b026ebaf9daef3f8e7385a8c899cff254bd689b
Rui Rocha <rocha.rui@siemens-logistics.com>
2021-04-06 15:56:17 +0100

    fourth commit

--- src/main/java/com/demo/App.java
+++ src/main/java/com/demo/App.java
@@ -1,12 +1,15 @@
 package com.demo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.client.RestTemplate;
@@ -14,11 +17,24 @@ import org.springframework.web.client.RestTemplate;
 @Controller
 @EnableScheduling
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer implements WebApplicationInitializer    {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return configureApplication(builder);
+    }
 
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        configureApplication(new SpringApplicationBuilder()).run(args);
     }
+
+    private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+        return builder.sources(App.class).bannerMode(Banner.Mode.OFF);
+    }
+//@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(Application.class);
+//	}
 
     @Bean
     protected RestTemplate restTemplate() {
