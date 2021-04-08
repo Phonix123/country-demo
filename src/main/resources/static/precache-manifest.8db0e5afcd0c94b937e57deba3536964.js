617d638271ee8bb5d2ee3f9d52b259d1e930835c
Rui Rocha <rocha.rui@siemens-logistics.com>
2021-04-08 14:21:03 +0100

    updated frontend

---
+++ src/main/resources/static/precache-manifest.8db0e5afcd0c94b937e57deba3536964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8697f91f0a3333eabf64",
+    "url": "/css/app.bd3d7d79.css"
+  },
+  {
+    "revision": "cd9ac7d280e71c83785b",
+    "url": "/css/chunk-vendors.40574e05.css"
+  },
+  {
+    "revision": "493aa5b962936ddc7fcc0b9844c9c509",
+    "url": "/images/background.png"
+  },
+  {
+    "revision": "d00f3ca46c0c1e0137d23f7702966df2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8697f91f0a3333eabf64",
+    "url": "/js/app.7f9ff404.js"
+  },
+  {
+    "revision": "cd9ac7d280e71c83785b",
+    "url": "/js/chunk-vendors.922b4529.js"
+  },
+  {
+    "revision": "f5abde0d2f4caf621e2533335cb6258c",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
